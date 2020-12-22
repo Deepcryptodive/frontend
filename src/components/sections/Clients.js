d66494cc523a6faa78f97c18ec463a96f068c5c8
Rachel <rachblondon@gmail.com>
2020-12-22 12:17:18 +0100

    the graph logo

--- src/components/sections/Clients.js
+++ src/components/sections/Clients.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import classNames from 'classnames';
-import { SectionProps } from '../../utils/SectionProps';
-import Image from '../elements/Image';
+import React from "react";
+import classNames from "classnames";
+import { SectionProps } from "../../utils/SectionProps";
+import Image from "../elements/Image";
 
 const propTypes = {
-  ...SectionProps.types
-}
+  ...SectionProps.types,
+};
 
 const defaultProps = {
-  ...SectionProps.defaults
-}
+  ...SectionProps.defaults,
+};
 
 class Clients extends React.Component {
-
   render() {
     const {
       className,
@@ -26,50 +25,56 @@ class Clients extends React.Component {
     } = this.props;
 
     const outerClasses = classNames(
-      'clients section reveal-fade',
-      topOuterDivider && 'has-top-divider',
-      bottomOuterDivider && 'has-bottom-divider',
-      hasBgColor && 'has-bg-color',
-      invertColor && 'invert-color',
+      "clients section reveal-fade",
+      topOuterDivider && "has-top-divider",
+      bottomOuterDivider && "has-bottom-divider",
+      hasBgColor && "has-bg-color",
+      invertColor && "invert-color",
       className
     );
 
     const innerClasses = classNames(
-      'clients-inner section-inner',
-      topDivider && 'has-top-divider',
-      bottomDivider && 'has-bottom-divider'
+      "clients-inner section-inner",
+      topDivider && "has-top-divider",
+      bottomDivider && "has-bottom-divider"
     );
 
     return (
-      <section
-        {...props}
-        className={outerClasses}
-      >
+      <section {...props} className={outerClasses}>
         <div className="container">
-          <div
-            className={innerClasses}
-          >
+          <div className={innerClasses}>
             <ul className="list-reset">
               <li className="reveal-rotate-from-left" data-reveal-delay="100">
                 <Image
-                  src={require('./../../assets/images/aave-logo.svg')}
+                  src={require("./../../assets/images/aave-logo.svg")}
                   alt="Client 01"
                   width={124}
-                  height={24} />
+                  height={24}
+                />
               </li>
               <li className="reveal-rotate-from-left" data-reveal-delay="150">
                 <Image
-                  src={require('./../../assets/images/ethereum.svg')}
+                  src={require("./../../assets/images/ethereum.svg")}
                   alt="Client 02"
                   width={83}
-                  height={30} />
+                  height={30}
+                />
               </li>
               <li className="reveal-rotate-from-left" data-reveal-delay="300">
                 <Image
-                  src={require('./../../assets/images/gitcoin.svg')}
+                  src={require("./../../assets/images/gitcoin.svg")}
                   alt="Client 03"
                   width={125}
-                  height={30} />
+                  height={30}
+                />
+              </li>
+              <li className="reveal-rotate-from-left" data-reveal-delay="300">
+                <Image
+                  src={require("./../../assets/images/the-graph.svg")}
+                  alt="Client 03"
+                  width={60}
+                  height={30}
+                />
               </li>
             </ul>
           </div>
