cfb5ad8e7e46b43e6c650c7e613f134dbeaa62d7
GoodGhosting <boo@goodghosting.com>
2020-12-31 16:22:18 +0100

    contract and the graph url to admin

--- src/components/sections/LiveGame.js
+++ src/components/sections/LiveGame.js
@@ -132,6 +132,42 @@ export default (props) => (
                 You shouldn't need to call these functions, as the GoodGhosting
                 team will do. However for transparency we have made them open.
               </p>
+              <p>
+                Contract Address:{" "}
+                <a
+                  href={`https://kovan.etherscan.io/address/${process.env.REACT_APP_GG_CONTRACT}`}
+                  rel="noopener noreferrer nofollow"
+                  target="_blank"
+                >
+                  <span
+                    style={{
+                      backgroundColor: "rgb(246, 248, 254)",
+                      borderRadius: "3px",
+                    }}
+                  >
+                    {" "}
+                    {process.env.REACT_APP_GG_CONTRACT}
+                  </span>
+                </a>
+              </p>
+              <p>
+                The Graph URL:{" "}
+                <a
+                  href={process.env.REACT_APP_GRAPH_URL}
+                  rel="noopener noreferrer nofollow"
+                  target="_blank"
+                >
+                  <span
+                    style={{
+                      backgroundColor: "rgb(246, 248, 254)",
+                      borderRadius: "3px",
+                    }}
+                  >
+                    {" "}
+                    {process.env.REACT_APP_GRAPH_URL}
+                  </span>
+                </a>
+              </p>
             </TabContent>
           </Tabs>
         </Col>
